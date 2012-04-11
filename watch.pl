7c7ad86761014e350b8a21382711087141b414a2
Tom Matthews <tom@inkhorn.net>
2012-04-11 13:28:56 +0100

    rework header text

--- watch.pl
+++ watch.pl
@@ -1,6 +1,17 @@
 #!/usr/bin/env perl
-# PERL implementation of the Linux watch utility
-# 10/04/2012
+# watch -- execute a program repeatedly, displaying output fullscreen
+#
+# Based on the original 1991 'watch' by Tony Rems <rembo@unisoft.com>
+# (with mods and corrections by Francois Pinard).
+#
+# Substantially reworked, new features (differences option, SIGWINCH
+# handling, unlimited command length, long line handling) added Apr 1999 by
+# Mike Coleman <mkc@acm.org>.
+#
+# Changes by Albert Cahalan, 2002-2003.
+#
+# PERL implementation by Tom Matthews, 2012.
+#
 
 my @lines = `ls -l`;
 
