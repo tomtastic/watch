9770c39b58d86e3bce4a25c5cee9fb7946f24a02
Tom Matthews <tom@inkhorn.net>
2012-04-11 13:48:44 +0100

    trim example code

--- watch.pl
+++ watch.pl
@@ -13,17 +13,6 @@
 # PERL implementation by Tom Matthews, 2012.
 #
 
-my @lines = `ls -l`;
-
-while defined(my $line = <>) {
-    my $start = $-[0];
-    my $stop  = $+[0];
-    my $underline = ( ' ' x $-[0] ) . ( '^' x ($stop - $start) );
-
-    print $line;
-    print $underline, "\n\n";
-}
-
 sub usage() {
     print STDERR "Usage: $0 [-dhntv] [--differences[=cumulative]] [--help] [--interval=<n>] [--no-title] [--version] <command>\n";
     exit 1
